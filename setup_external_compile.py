d0b16243adb97852310da29a1db7de2828e0b84c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 03:54:16 +0000

    Adding numeric_owner as keyword arguement

--- setup_external_compile.py
+++ setup_external_compile.py
@@ -57,7 +57,7 @@ def tar_extract(tar_file, target):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tgz, target)
